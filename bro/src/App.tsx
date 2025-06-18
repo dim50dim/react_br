5dced9a5cf41618b896d42d3e36115e8170f4fba
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-18 22:04:25 +0200

    removed types of name - and it works.

--- bro/src/App.tsx
+++ bro/src/App.tsx
@@ -7,10 +7,10 @@ function App() {
 
   return (
     <>
-<Student name:string ='Mike' age:number={54} isStudent:boolean={false}/>
-<Student name:string='Patrick' age:number={33} isStudent={false}/>
-<Student name:string ='Tom' age={14} isStudent={true}/>
-<Student name:string='Sandy' age={42} isStudent={true}/>
+<Student name ='Mike' age:number={54} isStudent:boolean={false}/>
+<Student name='Patrick' age:number={33} isStudent={false}/>
+<Student name ='Tom' age={14} isStudent={true}/>
+<Student name='Sandy' age={42} isStudent={true}/>
     </>
   )
 }
--- bro/src/components/Student.tsx
+++ bro/src/components/Student.tsx
@@ -5,7 +5,7 @@ export default function Student(props) {
          <div className="student">
             <p>Name : {props.name} </p>
             <p>Age : {props.age} </p>
-            <p>Student : {props.isStudent} </p>
+            <p>Student : {props.isStudent ? 'Yes' : 'No'} </p>
          </div>
         </>
     )
