96f79444db9535b0c4c87454f29f18ca3bb07edf
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-18 21:56:47 +0200

    added tying to names.

--- bro/src/App.tsx
+++ bro/src/App.tsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
 <Student name:string ='Mike' age:number={54} isStudent:boolean={false}/>
-<Student name='Patrick' age:number={33} isStudent={false}/>
-<Student name='Tom' age={14} isStudent={true}/>
-<Student name='Sandy' age={42} isStudent={true}/>
+<Student name:string='Patrick' age:number={33} isStudent={false}/>
+<Student name:string ='Tom' age={14} isStudent={true}/>
+<Student name:string='Sandy' age={42} isStudent={true}/>
     </>
   )
 }
