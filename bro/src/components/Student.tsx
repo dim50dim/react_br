ab8295449a227516db419cf7df8fd1f4fbf95829
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-18 22:00:25 +0200

    added new props - however - it does not work.

--- bro/src/components/Student.tsx
+++ bro/src/components/Student.tsx
@@ -3,7 +3,9 @@ export default function Student(props) {
     return (
         <>
          <div className="student">
-
+            <p>Name : {props.name} </p>
+            <p>Age : {props.age} </p>
+            <p>Student : {props.isStudent} </p>
          </div>
         </>
     )
