a10976709b3154242b17447d6bac0e7bbc180994
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-18 11:47:08 +0200

    install type from npm

--- bro/src/App.tsx
+++ bro/src/App.tsx
@@ -7,7 +7,10 @@ function App() {
 
   return (
     <>
-<Student name ='Mike' age={54} isStudent={false}/>
+<Student name:string ='Mike' age:number={54} isStudent:boolean={false}/>
+<Student name='Patrick' age:number={33} isStudent={false}/>
+<Student name='Tom' age={14} isStudent={true}/>
+<Student name='Sandy' age={42} isStudent={true}/>
     </>
   )
 }
--- bro/src/components/Student.tsx
+++ bro/src/components/Student.tsx
@@ -1,11 +1,16 @@
-export default function Student(props:string) {
+
+export default function Student(props) {
     return (
         <>
-         <div>
+         <div className="student">
             <p>Name: {props.name}</p>
             <p>Age: {props.age} </p>
             <p>Student : {props.isStudent ? 'Yes' : 'No'} </p>
          </div>
         </>
     )
+}
+
+Student.propTypes = {
+
 }
