35dab3663bb733a6fe7b181bd59e41eae72448b1
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-18 10:30:26 +0200

    deleted props from Header component.

--- bro/src/App.tsx
+++ bro/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 
 
@@ -6,7 +7,8 @@ function App() {
 
   return (
     <>
-    <Header age='44'/>
+    <Header/>
+    <Footer/>
     </>
   )
 }
--- bro/src/components/Header.tsx
+++ bro/src/components/Header.tsx
@@ -1,4 +1,4 @@
-export default function Header(props: number) {
+export default function Header() {
     return(
         <>
              <header>
